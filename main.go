ea1e7d45ab29f026ef43a53a2b788865f7d0ed60
nichigovsky <inichigovsky@gmail.com>
2025-11-23 22:37:57 +0300

    feat: create user input func, calculate value func

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+func userInput() (string, string, float64) {
+	var valueFrom string
+	var valueTo string
+	var value float64
+
+	fmt.Print("Из какой валюты:")
+	fmt.Scan(&valueFrom)
+
+	fmt.Print("В какую валюту:")
+	fmt.Scan(&valueTo)
+	
+	fmt.Print("Сколько:")
+	fmt.Scan(&value)
+
+	return valueFrom, valueTo, value
+}
+
+func calculateValue(valueFrom string, valueTo string, value float64) {
+
+}
+
 func main(){
 	const USDtoEUR = 0.87
 	const USDtoRUB = 80.05
@@ -9,4 +30,7 @@ func main(){
 	EURtoRUB := EUR / USDtoEUR * USDtoRUB
 
 	fmt.Printf("EUR in RUB: %.2f", EURtoRUB)
+
+	valueFrom, valueTo, value := userInput();
+	calculateValue(valueFrom, valueTo, value)
 }
